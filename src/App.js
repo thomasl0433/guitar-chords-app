7689f05c6275ef80e30ae5c46cb5e4f9b11e1b9f
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-07-01 16:00:45 -0400

    progress on nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Body from "./components/Body";
+//import Body from "./components/Body";
 import NavBar from './components/Navbar/Navbar'
 //import Header from './components/Header'
 //import Reference from "./components/Reference";
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Body />
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import { MenuItems } from "./MenuItems";
 
+// using class component is ideal for using State
 export default class NavBar extends React.Component {
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <nav className="NavBarItems">
+        <h1 className="navbar-logo">React</h1>
+        <div className="menu-icon"></div>
+        <ul>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                    {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
 }
