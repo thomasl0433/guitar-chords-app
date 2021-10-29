8ef8acd95d7088ce4ecfcb55e88a117ab7adb779
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-10-29 19:12:01 -0400

    Changed logo. Added tailwindcss

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,43 @@
 import React from "react";
 import { MenuItems } from "./MenuItems";
+import "./Navbar.css";
+import { Button } from "../Button";
 
 // using class component is ideal for using State
 export default class NavBar extends React.Component {
+  state = {
+    clicked: false,
+  };
+
+  handleClick = (event) => {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  };
+
   render() {
     return (
       <nav className="NavBarItems">
-        <h1 className="navbar-logo">React</h1>
-        <div className="menu-icon"></div>
-        <ul>
+        <h1 className="navbar-logo">
+          Guitar Chord Dictionary
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
                 <a className={item.cName} href={item.url}>
-                    {item.title}
+                  {item.title}
                 </a>
               </li>
             );
           })}
         </ul>
+        <Button>Sign Up</Button>
       </nav>
     );
   }
