5195a853d57c8a92615f05fffbb0d5f5fb54f8c8
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-11-01 19:00:22 -0400

    removed old nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-//import Body from "./components/Body";
-import NavBar from './components/Navbar/Navbar'
 import { MainSection } from "./components/MainSection/MainSection";
-//import Header from './components/Header'
-//import Reference from "./components/Reference";
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <MainSection />
     </div>
   );
