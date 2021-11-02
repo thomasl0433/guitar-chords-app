c7637774f3e38b420520fb6c27bd5f15635fb842
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2021-11-01 20:39:24 -0400

    Added navbar and dummy form field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
-import { MainSection } from "./components/MainSection/MainSection";
+import NavBar from "./components/NavBar";
+import Form from "./components/Form";
 
 
 function App() {
   return (
-    <>
+    <div>
       <NavBar />
-      <MainSection />
-    </>
+      <Form />
+    </div>
   );
 }
 
